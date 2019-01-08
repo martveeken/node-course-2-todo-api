ed6152bbda04e6c30212602036bfec988805ef3f
martveeken <mart@inkstrategy.com>
2019-01-08 21:03:11 +0100

    PATCH update for todos

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
@@ -57,6 +58,32 @@ app.delete('/todos/:id', (req, res) => {
     }
     res.send({ todo });
   }).catch((e) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findOneAndUpdate({ _id: id }, { $set: body }).then((todo) => {
+    console.log(todo);
+    if (!todo) {
+      return res.status(400).send();
+    }
+    res.send({ todo });
+  }).catch((e) => {
+    res.status(404).send(e);
+  });
 });
 
 app.listen(port, () => {
