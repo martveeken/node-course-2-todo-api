4114bc810f58ae87c08ee20429f28488e5ffd5cc
martveeken <mart@inkstrategy.com>
2019-01-10 16:31:30 +0100

    add tests for POST users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -177,21 +177,62 @@ describe('/POST /users', () => {
           expect(user).toBeTruthy();
           //expect(user.password).toBe(!password);
           done();
-        });
+        }).catch((e) => done(e));
+      });
+  });
+});
+
+describe('/POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeTruthy();
+      })
+      .end((err, res) => {
+        if (!err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
       });
   });
 
-  // it('should return validation errors if request invalid', (done) => {
-  //
-  // });
-  //
-  // it('should not create if email in use', (done) => {
-  //
-  // });
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'knetter'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeFalsy();
+      })
+      .end((err, res) => {
+        if (!err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
 });
 
-
-//
 // describe('PATCH /todos/:id', () => {
 //   it('should return 200 and patched', (done) => {
 //     const hexId = dummyTodos[0]._id.toHexString();
