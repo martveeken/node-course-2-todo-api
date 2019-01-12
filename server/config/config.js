dd34c4f0985d77dab0459c51a5e1592d6fed73f6
martveeken <mart@inkstrategy.com>
2019-01-12 16:47:47 +0100

    add config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
 const env = process.env.NODE_ENV || 'development';
+const config = require('./config.json');
+
 console.log('env ******', env);
 
 if (env === 'development' || 'test') {
-  const config = require('./config.json');
-
   const envConfig = config[env];
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
